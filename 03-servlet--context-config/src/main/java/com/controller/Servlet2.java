f178b398048d0d38a1d54718bf2bded5ee73c5c6
Manish Bhatt <manishbhatt594@gmail.com>
2026-05-08 17:07:21 +0530

    Add two Servlets

---
+++ 03-servlet--context-config/src/main/java/com/controller/Servlet2.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet2 extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+}
