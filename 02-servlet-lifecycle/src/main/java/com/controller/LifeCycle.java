8e14a63492d0a59b5ee49ad6e9ccbb8416af9f1a
Manish Bhatt <manishbhatt594@gmail.com>
2026-05-08 14:55:36 +0530

    Print logs in various Servlet life-cycle methods

--- 02-servlet-lifecycle/src/main/java/com/controller/LifeCycle.java
+++ 02-servlet-lifecycle/src/main/java/com/controller/LifeCycle.java
@@ -10,30 +10,40 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LifeCycle extends HttpServlet {
 
+	static {
+		System.out.println("LifeCycle Servlet - LOADING. Inside LifeCycle class's static block.");
+	}
+
 	public LifeCycle() {
+		System.out.println("LifeCycle Servlet - INSTANTIATION. Inside LifeCycle class's default constructor.");
 	}
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
+		System.out.println("LifeCycle Servlet - INIT. Inside LifeCycle class's init() method.");
 	}
 
 	@Override
 	public void destroy() {
+		System.out.println("LifeCycle Servlet - DESTROY. Inside LifeCycle class's destroy() method.");
 	}
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		System.out.println("LifeCycle Servlet - SERVICE. Inside LifeCycle class's service() method.");
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		System.out.println("LifeCycle Servlet - doGET. Inside LifeCycle class's doGet() method.");
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		System.out.println("LifeCycle Servlet - doPOST. Inside LifeCycle class's doPost() method.");
 	}
 
 }
