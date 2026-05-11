6bf13405152f55cd2b8e4f2fda33623d8cdc3d13
Manish Bhatt <manishbhatt594@gmail.com>
2026-05-11 15:57:53 +0530

    Change url-pattern for TestMvc servlet

--- 07-mvc-flow/src/main/java/com/controller/TestMvc.java
+++ 07-mvc-flow/src/main/java/com/controller/TestMvc.java
@@ -12,6 +12,9 @@ public class TestMvc extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		System.out.println("TestMvc doGet()...");
+
 	}
 
 }
