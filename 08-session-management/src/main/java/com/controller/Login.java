86e6a51818a5ba9fdad9dcdb9d8f6188639795c8
Manish Bhatt <manishbhatt594@gmail.com>
2026-05-12 20:20:28 +0530

    Add views, Servlets for Login (functionality pending)

---
+++ 08-session-management/src/main/java/com/controller/Login.java
@@ -0,0 +1,21 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+	}
+
+}
