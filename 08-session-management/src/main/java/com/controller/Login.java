6ca98d74284efb22d98f5d62388ade1f0a1e5687
Manish Bhatt <manishbhatt594@gmail.com>
2026-05-13 17:11:40 +0530

    On login credentials match, Put username in session
    Also check if username exists in session, on doing a GET

--- 08-session-management/src/main/java/com/controller/Login.java
+++ 08-session-management/src/main/java/com/controller/Login.java
@@ -6,8 +6,25 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Login extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+
+		String data = (String) session.getAttribute("data");
+
+		if (data != null) {
+			request.getRequestDispatcher("/WEB-INF/view/home.jsp").forward(request, response);
+		} else {
+			request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+		}
+
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -17,6 +34,10 @@ public class Login extends HttpServlet {
 		String password = request.getParameter("password");
 
 		if (username.equals("raju") && password.equals("123456")) {
+
+			HttpSession session = request.getSession();
+			session.setAttribute("data", username);
+
 			request.getRequestDispatcher("/WEB-INF/view/home.jsp").forward(request, response);
 		} else {
 			request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
