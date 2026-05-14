8f59f16bcbccfd54b8180f92343fa219d7d1cec7
Manish Bhatt <manishbhatt594@gmail.com>
2026-05-14 16:43:10 +0530

    Print logs in Listener methods ctxDestroyed & ctxInitialized

--- 09-listener/src/main/java/com/test/listener/Startup.java
+++ 09-listener/src/main/java/com/test/listener/Startup.java
@@ -7,10 +7,14 @@ public class Startup implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
+		// Prints: Application is shutting down... [/09-listener]
+		System.out.println("Application is shutting down... [" + sce.getServletContext().getContextPath() + "]");
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
+		// Prints: Application is starting up... [/09-listener]
+		System.out.println("Application is starting up... [" + sce.getServletContext().getContextPath() + "]");
 	}
 
 }
