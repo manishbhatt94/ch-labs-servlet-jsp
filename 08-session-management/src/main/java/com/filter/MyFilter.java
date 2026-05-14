2c6eedd40ad56bece3d172dfd6333f408c274a28
Manish Bhatt <manishbhatt594@gmail.com>
2026-05-14 17:23:11 +0530

    Keep key methods (init, doFilter & destroy) in Filter class
    Remove rest all unnecessary auto-generated comments, annotation,
    constructor definition.
    Note: We can remove annotation since the filter mapping is done in
    web.xml

--- 08-session-management/src/main/java/com/filter/MyFilter.java
+++ 08-session-management/src/main/java/com/filter/MyFilter.java
@@ -1,52 +1,31 @@
 package com.filter;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 
-/**
- * Servlet Filter implementation class MyFilter
- */
-@WebFilter("/MyFilter")
 public class MyFilter extends HttpFilter implements Filter {
-       
-    /**
-     * @see HttpFilter#HttpFilter()
-     */
-    public MyFilter() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see Filter#destroy()
-	 */
+	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 
 		// pass the request along the filter chain
 		chain.doFilter(request, response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
+	@Override
 	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
 	}
 
 }
