b47ead4403cd138070074f3da90651b4ed3c4e9b
Manish Bhatt <manishbhatt594@gmail.com>
2026-05-19 14:35:59 +0530

    Provide URL mapping for Test Servlet using WebServlet annotation

--- 13-annotations/src/main/java/com/controller/Test.java
+++ 13-annotations/src/main/java/com/controller/Test.java
@@ -1,33 +1,36 @@
 package com.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Test
- */
-@WebServlet("/Test")
+@WebServlet("/test-servlet")
 public class Test extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Test() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+
+		// @formatter:off
+		out.println("<!DOCTYPE html>"
+				+ "<html lang=\"en-IN\"><head>"
+				+ "<meta charset=\"UTF-8\">"
+				+ "<title>Test Servlet</title></head>"
+				+ "<body>"
+				+ "<header><h1>Welcome to Test Servlet</h1></header>"
+				+ "<main><p>Goodbye!</p></main>"
+				+ "</body></html>");
+		// @formatter:on
+
 	}
 
 }
---
+++ 13-annotations/src/main/webapp/index.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Index page</title>
+</head>
+<body>
+
+	<header>
+		<h1>Index page</h1>
+		<nav>
+			<ul>
+				<li><a href="test-servlet">Test Servlet</a></li>
+			</ul>
+		</nav>
+	</header>
+	
+	<main>
+		<p>This is the index page.</p>
+	</main>
+
+</body>
+</html>
