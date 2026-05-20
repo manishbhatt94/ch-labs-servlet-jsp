03ee06ec0454b1b57cab8214cb2a837ba58f0694
Manish Bhatt <manishbhatt594@gmail.com>
2026-05-20 16:22:13 +0530

    EMS: Setup JDBC connection pool on ServletContextListener initialized

---
+++ 14-proj-employee-management/src/main/java/com/dao/ConnectionFactory.java
@@ -0,0 +1,56 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Objects;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class ConnectionFactory {
+
+	private static HikariDataSource hikariDataSource;
+
+	public static void init(String driver, String url, String username, String password) {
+
+		System.out.println("ConnectionFactory#init(): Begin DB connection pool initialization work!");
+		try {
+			Class.forName(driver);
+			System.out.println("ConnectionFactory#init(): Loaded JDBC driver class: " + driver);
+
+			System.out.println("ConnectionFactory#init(): Creating HikariConfig object...");
+			HikariConfig hikariConfig = new HikariConfig();
+			System.out.println("ConnectionFactory#init(): Created HikariConfig object. "
+					+ "Setting JDBC URL, username and password");
+
+			hikariConfig.setJdbcUrl(url);
+			hikariConfig.setUsername(username);
+			hikariConfig.setPassword(password);
+			System.out.println("ConnectionFactory#init(): HikariConfig object configured. "
+					+ "Creating HikariDataSource object...");
+
+			hikariDataSource = new HikariDataSource(hikariConfig);
+			System.out.println("ConnectionFactory#init(): HikariDataSource object created. "
+					+ "DB connection pool ready to be used!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static Connection getConnection() throws SQLException {
+		Objects.requireNonNull(hikariDataSource,
+				"Connection pool is not initialized. Call ConnectionFactory.init() first.");
+		return hikariDataSource.getConnection();
+	}
+
+	public static void closePool() {
+		Objects.requireNonNull(hikariDataSource, "Connection pool is not initialized. "
+				+ "Cannot call close on HikariDataSource reference having null value.");
+		System.out.println(
+				"ConnectionFactory#closePool(): Closing HikariDataSource " + "object to destroy DB connection pool...");
+		hikariDataSource.close();
+	}
+
+}
---
+++ 14-proj-employee-management/src/main/java/com/listener/EmployeeListener.java
@@ -0,0 +1,99 @@
+package com.listener;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import com.dao.ConnectionFactory;
+
+@WebListener
+public class EmployeeListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+
+		mysqlPoolDestroy();
+
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+
+		ServletContext servletContext = sce.getServletContext();
+
+		mysqlPoolSetup(servletContext);
+
+	}
+
+	private void mysqlPoolSetup(ServletContext servletContext) {
+		try {
+			System.out.println("EmployeeListener#mysqlPoolSetup(): Reading config.properties file");
+
+			// ####### Construct Properties object from config.properties file #######
+
+			// ======= Method 1: =======
+			// == Construct InputStream ourselves to the config.properties file ==
+			// (Commenting out this method)
+
+			/*
+			 * @formatter:off
+			// First: Find realPath of the config.properties file.
+			String realPath = servletContext.getRealPath("/WEB-INF/config/config.properties");
+			System.out.println("Real Path [for config.properties file]: " + realPath);
+			// Then: Construct InputStream to the config.properties file.
+			FileInputStream fis = new FileInputStream(realPath); // This throws checked "FileNotFoundException" which we
+																	// have to handle
+			 * @formatter:on
+			*/
+
+			// ======= Method 2: =======
+			// == Let ServletContext construct InputStream to the config.properties file ==
+			// == using getResourceAsStream() method of ServletContext ==
+
+			InputStream iStream = servletContext.getResourceAsStream("/WEB-INF/config/config.properties");
+			System.out.println("EmployeeListener#mysqlPoolSetup(): Obtained InputStream to config.properties file");
+
+			// ### Create Properties object, load it using the InputStream object ###
+			Properties properties = new Properties();
+			properties.load(iStream); // This throws checked "IOException" which we have to handle
+			System.out.println(
+					"EmployeeListener#mysqlPoolSetup(): Loaded config.properties " + "file into Properties object");
+
+			String driver = properties.getProperty("mysql.driver");
+			String url = properties.getProperty("mysql.url");
+			String username = properties.getProperty("mysql.username");
+			String password = properties.getProperty("mysql.password");
+			System.out.println("EmployeeListener#mysqlPoolSetup(): Connection details - [driver(" + driver + "), url("
+					+ url + "), username(" + username + "), password(" + password + ")]");
+			System.out.println("EmployeeListener#mysqlPoolSetup(): Call ConnectionFactory.init() "
+					+ "with MySQL connection details");
+
+			ConnectionFactory.init(driver, url, username, password);
+
+			// Test code to verify that we are gettting a valid Connection object from the
+			// connection pool. Just printing the Connection object to console.
+			/*
+			 * @formatter:off
+			System.out.println(
+					"EmployeeListener#mysqlPoolSetup(): Testing connection pool by getting a Connection object");
+			System.out.println("Connection object: " + ConnectionFactory.getConnection());
+			 * @formatter:on
+			 */
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void mysqlPoolDestroy() {
+		System.out.println("EmployeeListener#mysqlPoolDestroy(): Calling ConnectionFactory.closePool() "
+				+ "to destroy MySQL connection pool");
+		ConnectionFactory.closePool();
+	}
+
+}
